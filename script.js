e9a19d9ac31d2d20fa0d54629f5bc63731e25dcf
Robby <rramirez728@gmail.com>
2026-07-02 19:39:00 -0500

    Fixed result text size

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-    function calculateOG() {
-      let honey = Number(document.getElementById("honey").value);
-      let gallons = Number(document.getElementById("gallons").value);
-
-      let gravityPoints = (honey * 35) / gallons;
-      let og = 1 + (gravityPoints / 1000);
-
-      document.getElementById("result").innerText =
-        honey + " lb of honey in " + gallons + " gallon(s) gives an estimated OG of " + og.toFixed(3);
-    }
-
-    function calculateOG() {
+function calculateOG() {
     let honey = Number(document.getElementById("honey").value);
     let gallons = Number(document.getElementById("gallons").value);
 
@@ -17,15 +6,34 @@
     let og = 1 + (gravityPoints / 1000);
 
     document.getElementById("result").innerText =
-      honey + " lb of honey in " + gallons + " gallon(s) gives an estimated OG of " + og.toFixed(3);
-  }
+        "Estimated Original Gravity: " + og.toFixed(3);
+}
 
-  function calculateABV() {
+function calculateABV() {
     let og = Number(document.getElementById("og").value);
     let fg = Number(document.getElementById("fg").value);
 
     let abv = (og - fg) * 131.25;
 
     document.getElementById("abvResult").innerText =
-      "Estimated ABV: " + abv.toFixed(2) + "%";
-  }
+        "Estimated ABV: " + abv.toFixed(2) + "%";
+}
+
+function calculateHoneyNeeded() {
+    let targetOg = Number(document.getElementById("targetOg").value);
+    let gallons = Number(document.getElementById("targetGallons").value);
+
+    // Validation
+    if (!targetOg || !gallons) {
+        document.getElementById("honeyNeededResult").innerText =
+            "Please enter a target gravity and batch size.";
+        return;
+    }
+
+    // Calculation
+    let gravityPoints = (targetOg - 1) * 1000;
+    let honeyNeeded = (gravityPoints * gallons) / 35;
+
+    document.getElementById("honeyNeededResult").innerText =
+        "Estimated Honey Needed: " + honeyNeeded.toFixed(2) + " lb";
+}
