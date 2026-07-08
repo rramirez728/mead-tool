a8d663db59e30d708094c64bb017ca0575604cbc
Robby <rramirez728@gmail.com>
2026-07-07 20:38:22 -0500

    Add water calculator

--- script.js
+++ script.js
@@ -37,3 +37,20 @@ function calculateHoneyNeeded() {
     document.getElementById("honeyNeededResult").innerText =
         "Estimated Honey Needed: " + honeyNeeded.toFixed(2) + " lb";
 }
+
+function calculateWaterNeeded() {
+    let honey = Number(document.getElementById("waterHoney").value);
+    let targetOg = Number(document.getElementById("waterTargetOg").value);
+
+    if (!honey || !targetOg) {
+        document.getElementById("waterNeededResult").innerText =
+            "Please enter honey amount and target gravity.";
+        return;
+    }
+
+    let gravityPoints = (targetOg - 1) * 1000;
+    let gallons = (honey * 35) / gravityPoints;
+
+    document.getElementById("waterNeededResult").innerText =
+        "Estimated Total Batch Volume: " + gallons.toFixed(2) + " gallons";
+}
